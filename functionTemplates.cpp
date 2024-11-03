bed4e5763ed47e18471790aac3fee184bd3de4a7
kdowd <kevin.dowd@gmail.com>
2024-11-04 12:24:40 +1300

    clean up

--- functionTemplates.cpp
+++ functionTemplates.cpp
@@ -4,22 +4,16 @@
 using namespace std;
 
 
-// overloading
-//int add(int a, int b) {
-//	cout << "++++++ " << endl;
-//	return (a + b);
-//}
-//
-//float add(float a, float b) {
-//	return (a + b);
-//}
-
-
-template <typename t>
-t add(t a, t b) {
-	cout << typeid(t).name() << endl;
+// traditional overloading
+int add(int a, int b) {
+	cout << "++++++ " << endl;
 	return (a + b);
 }
+
+float add(float a, float b) {
+	return (a + b);
+}
+
 
 
 
@@ -30,6 +24,6 @@ int main()
 	float b = 8.4;
 	cout << add(a, b) << endl;
 
-	cout << add(1.2342342342342344, 5.3453453454353453453) << endl;
+	// try a double
 
 }
